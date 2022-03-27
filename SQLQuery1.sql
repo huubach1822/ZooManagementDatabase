e8ef273939735b81ce633776f7567af1e40cf8a2
huubach1822 <monsterxyz02@gmail.com>
2022-03-27 21:59:24 +0700

    tao bang animal

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,23 @@
 create database ZooManagement
 go
+
+create table Animal
+(
+	ID int not null,
+	Vietnamese_name nvarchar(50) not null,
+	Specie_ID int,
+	Quantity int,
+	Red_list bit,
+	Sciene_name nvarchar(50),
+	English_name nvarchar(50),
+	Type_of_birth nvarchar(50),
+	Gender bit,
+	Date_of_joint datetime,
+	Origin nvarchar(50),
+	Feature nvarchar(50),
+	Date_of_birth datetime,
+	Picture nvarchar(50),
+	Age int,
+	constraint pk_ID primary key(ID),
+
+)
